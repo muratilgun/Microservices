c9e2f2a20c3ae67e752b6b341bd0929fc78f3886
muratilgun <muratilgun34@gmail.com>
2021-11-27 15:58:44 +0300

    CategoryService implemented

--- Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/ICategoryService.cs
@@ -1,11 +1,17 @@
-﻿using System;
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Model;
+using FreeCourse.Shared.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace FreeCourse.Services.Catalog.Services
 {
-    public interface ICategoryService
+    interface ICategoryService
     {
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> CreateAsync(CategoryDto categoryDto);
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
     }
 }
