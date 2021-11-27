1b3d277b7519a9d1744be6468dfd2967451b741a
muratilgun <muratilgun34@gmail.com>
2021-11-27 19:55:10 +0300

    GetByIdAsync

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -14,8 +14,9 @@ namespace FreeCourse.Services.Catalog.Services
     internal class CourseService : ICourseService
     {
         private readonly IMongoCollection<Course> _courseCollection;
+        private readonly IMongoCollection<Category> _categoryCollection;
 
-        
+
         private readonly IMapper _mapper;
 
         public CourseService( IMapper mapper,IDatabaseSettings databaseSettings)
@@ -23,17 +24,42 @@ namespace FreeCourse.Services.Catalog.Services
             var client = new MongoClient(databaseSettings.ConnectionStrings);
             var database = client.GetDatabase(databaseSettings.DatabaseName);
             _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
-            _mapper = mapper;
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CategoryCollectionName);
             _mapper = mapper;
         }
 
         public async Task<Response<List<CourseDto>>> GetAllAsync()
         {
             var courses = await _courseCollection.Find(course => true).ToListAsync();
+            
+            if (courses.Any())
+            {
+                foreach (var course in courses)
+                {
+                    course.Category = 
+                        await _categoryCollection
+                        .Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
+                }
+            }
+            else
+            {
+                courses = new List<Course>();
+            }
 
-            throw new NotImplementedException();
+            return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+            
         }
+        public async Task<Response<CourseDto>> GetByIdAsync(string id)
+        {
+            var course = await _courseCollection.Find<Course>(x => x.Id == id).FirstOrDefaultAsync();
+            if (course == null)
+            {
+                return Response<CourseDto>.Fail("Course not found", 404);
+            }
+            course.Category = await _categoryCollection.Find<Category>(x => x.Id == course.CategoryId).FirstAsync();
 
+            return Response<CourseDto>.Success(_mapper.Map<CourseDto>(course), 200);
+        }
         public async Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto)
         {
             throw new NotImplementedException();
@@ -51,10 +77,7 @@ namespace FreeCourse.Services.Catalog.Services
             throw new NotImplementedException();
         }
 
-        public async Task<Response<CourseDto>> GetByIdAsync(string id)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public async Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto)
         {
