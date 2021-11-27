d9697fccd2a9e3114d3500d98f2554dea78e7121
muratilgun <muratilgun34@gmail.com>
2021-11-27 16:46:12 +0300

    GetAllAsync

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Model;
+using FreeCourse.Services.Catalog.Settings;
+using FreeCourse.Shared.Dtos;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Services
+{
+    internal class CourseService : ICourseService
+    {
+        private readonly IMongoCollection<Course> _courseCollection;
+
+        
+        private readonly IMapper _mapper;
+
+        public CourseService( IMapper mapper,IDatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionStrings);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+            _courseCollection = database.GetCollection<Course>(databaseSettings.CourseCollectionName);
+            _mapper = mapper;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<CourseDto>>> GetAllAsync()
+        {
+            var courses = await _courseCollection.Find(course => true).ToListAsync();
+
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<NoContent>> DeleteAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public async Task<Response<List<CourseDto>>> GetAllByUserIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<CourseDto>> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
