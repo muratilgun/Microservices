6839196c3b029b4b8a104443bcf6cb0263083814
muratilgun <muratilgun34@gmail.com>
2021-11-27 20:56:05 +0300

    CustomBaseController

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using FreeCourse.Services.Catalog.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    internal class CoursesController : ControllerBase
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        //public async Task<IActionResult> GetById(string id)
+        //{
+        //    var response = await _courseService.GetByIdAsync(id);
+            
+        //}
+    }
+}
