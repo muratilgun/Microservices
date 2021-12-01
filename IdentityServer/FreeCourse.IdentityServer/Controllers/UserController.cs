043357bbc4e9c4b40191b996e44b02918866a299
muratilgun <muratilgun34@gmail.com>
2021-12-01 19:57:29 +0300

    SignUp User

--- IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/FreeCourse.IdentityServer/Controllers/UserController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FreeCourse.IdentityServer.Dtos;
+using FreeCourse.IdentityServer.Models;
+using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,5 +15,28 @@ namespace FreeCourse.IdentityServer.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpDto signUpDto)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = signUpDto.UserName,
+                Email = signUpDto.Email,
+                City = signUpDto.City
+            };
+            var result = await _userManager.CreateAsync(user, signUpDto.Password);
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=> x.Description).ToList(),400));
+            }
+            return NoContent();
+        }
     }
 }
--- IdentityServer/FreeCourse.IdentityServer/Dtos/SignUpDto.cs
+++ IdentityServer/FreeCourse.IdentityServer/Dtos/SignUpDto.cs
@@ -11,6 +11,5 @@ namespace FreeCourse.IdentityServer.Dtos
         public string Email { get; set; }
         public string Password { get; set; }
         public string City { get; set; }
-
     }
 }
