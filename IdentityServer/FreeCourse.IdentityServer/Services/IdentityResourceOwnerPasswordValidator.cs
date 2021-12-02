b45de4d64b64c9395b27030ecd691a5e30fc69eb
muratilgun <muratilgun34@gmail.com>
2021-12-03 00:05:26 +0300

    Create IdentityResourceOwnerPasswordValidator.cs

---
+++ IdentityServer/FreeCourse.IdentityServer/Services/IdentityResourceOwnerPasswordValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.IdentityServer.Services
+{
+    public class IdentityResourceOwnerPasswordValidator
+    {
+    }
+}
