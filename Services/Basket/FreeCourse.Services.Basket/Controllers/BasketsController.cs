f9e57fa071e6ebee1a62ab79080c90740ffcadd9
muratilgun <muratilgun34@gmail.com>
2021-12-06 23:57:14 +0300

    tested baskets getbasket endpoint

--- Services/Basket/FreeCourse.Services.Basket/Controllers/BasketsController.cs
+++ Services/Basket/FreeCourse.Services.Basket/Controllers/BasketsController.cs
@@ -26,6 +26,7 @@ namespace FreeCourse.Services.Basket.Controllers
         [HttpGet]
         public async Task<IActionResult> GetBasket()
         {
+            var claims = User.Claims;
             return CreateActionResultInstance(await _basketService.GetBasket(_sharedIdentityService.GetUserId));
         }
         [HttpPost]
