3c83540518aa2f4a28fc4577134c84adcb4802c1
muratilgun <muratilgun34@gmail.com>
2021-12-08 23:23:02 +0300

    Created DiscountService

---
+++ Services/Discount/FreeCourse.Services.Discount/Services/DiscountService.cs
@@ -0,0 +1,25 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        private readonly IConfiguration _configuration;
+        private IDbConnection _dbConnection;
+
+
+        public DiscountService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _dbConnection = new Npgsql.NpgsqlConnection(_configuration.GetConnectionString("PostgreSql"));
+        }
+
+   
+    }
+}
