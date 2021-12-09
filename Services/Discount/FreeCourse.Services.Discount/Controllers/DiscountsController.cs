4bf96b5133643770b501bf2f849ad0207ee32b32
muratilgun <muratilgun34@gmail.com>
2021-12-09 23:52:08 +0300

    Create DiscountsController

---
+++ Services/Discount/FreeCourse.Services.Discount/Controllers/DiscountsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Shared.ControllerBases;
+
+namespace FreeCourse.Services.Discount.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountsController : CustomBaseController
+    {
+    }
+}
