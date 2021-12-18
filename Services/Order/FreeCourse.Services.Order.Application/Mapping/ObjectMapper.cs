1cd501eee74312bc3aaa5e9b9e9a729a716925d6
muratilgun <muratilgun34@gmail.com>
2021-12-19 00:55:16 +0300

    Auto mapper

---
+++ Services/Order/FreeCourse.Services.Order.Application/Mapping/ObjectMapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace FreeCourse.Services.Order.Application.Mapping
+{
+    public class ObjectMapper
+    {
+        //lazy loading için yani sadece istenildiği zaman initialize etmek istiyorum.
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CustomMapping>();
+            });
+            return config.CreateMapper();
+        });
+    }
+}
