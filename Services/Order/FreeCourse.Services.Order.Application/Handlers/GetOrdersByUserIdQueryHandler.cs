0b22a9091a64bb38830d15324893ec9d4dfc6d78
muratilgun <muratilgun34@gmail.com>
2021-12-19 14:27:33 +0300

    Query and query handler
    MediatR

---
+++ Services/Order/FreeCourse.Services.Order.Application/Handlers/GetOrdersByUserIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FreeCourse.Services.Order.Application.Dtos;
+using FreeCourse.Services.Order.Application.Queries;
+using FreeCourse.Shared.Dtos;
+using MediatR;
+
+namespace FreeCourse.Services.Order.Application.Handlers
+{
+    public class GetOrdersByUserIdQueryHandler : IRequestHandler<GetOrdersByUserIdQuery,Response<List<OrderDto>>>
+    {
+        public async Task<Response<List<OrderDto>>> Handle(GetOrdersByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
