95af92b991ffb922d6ac956f587f9fb0d5804af1
muratilgun <muratilgun34@gmail.com>
2021-12-21 22:17:33 +0300

    Relevant settings were made in the configuration development file.

--- Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FreeCourse.Services.Catalog
 {
@@ -13,7 +16,24 @@ namespace FreeCourse.Services.Catalog
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host =CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var categoryService = serviceProvider.GetRequiredService<ICategoryService>();
+                if (!categoryService.GetAllAsync().Result.Data.Any())
+                {
+                    categoryService.CreateAsync(new CategoryDto
+                    {
+                        Name = "Asp.Net Core Kursu"
+                    }).Wait();
+                    categoryService.CreateAsync(new CategoryDto
+                    {
+                        Name = "Asp.Net Core API Kursu"
+                    }).Wait();
+                }
+            }
+            host.Run();;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
