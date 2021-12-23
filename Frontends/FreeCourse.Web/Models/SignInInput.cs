5304d45f76824d0b900676a6ba18d9a3af93d777
muratilgun <muratilgun34@gmail.com>
2021-12-23 21:57:37 +0300

    SingInInput

---
+++ Frontends/FreeCourse.Web/Models/SignInInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models
+{
+    public class SignInInput
+    {
+        [Display(Name = "Email adresiniz")]
+        public string Email { get; set; }
+        [Display(Name = "Şifreniz")]
+        public string Password { get; set; }
+        [Display(Name = "Beni hatırla")]
+        public bool IsRemember { get; set; }
+
+    }
+}
--- Frontends/FreeCourse.Web/Services/Interface/IIdentityService.cs
+++ Frontends/FreeCourse.Web/Services/Interface/IIdentityService.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FreeCourse.Shared.Dtos;
+using FreeCourse.Web.Models;
 using IdentityModel.Client;
 
 namespace FreeCourse.Web.Services.Interface
 {
     public interface IIdentityService
     {
-        Task<Response<bool>> SignIn();
+        Task<Response<bool>> SignIn(SignInInput input);
         Task<TokenResponse> GetAccessTokenByRefreshToken();
         Task RevokeRefreshToken();
     }
