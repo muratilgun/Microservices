0134015e1dbaade2888e0773c9818a2f3015edf2
muratilgun <muratilgun34@gmail.com>
2021-12-23 21:54:33 +0300

    IIdentityService & IdentityModel

---
+++ Frontends/FreeCourse.Web/Services/Interface/IIdentityService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Shared.Dtos;
+using IdentityModel.Client;
+
+namespace FreeCourse.Web.Services.Interface
+{
+    public interface IIdentityService
+    {
+        Task<Response<bool>> SignIn();
+        Task<TokenResponse> GetAccessTokenByRefreshToken();
+        Task RevokeRefreshToken();
+    }
+}
