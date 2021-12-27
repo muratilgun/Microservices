529fc37c92b44519195b558369db3d6131a7f7ae
muratilgun <muratilgun34@gmail.com>
2021-12-27 23:18:02 +0300

    signin endpoint && signin.cshtml

---
+++ Frontends/FreeCourse.Web/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
