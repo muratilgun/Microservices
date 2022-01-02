d3c132fb198aa256227a6a874a65b67613aeaa46
muratilgun <muratilgun34@gmail.com>
2022-01-02 22:45:02 +0300

    ICatalogService
    GetAllCourseAsync, GetAllCourseByUserIdAsync, GetByCourseIdAsync, CreateCourseAsync, UpdateCourseAsync DeleteCourseAsync

--- Frontends/FreeCourse.Web/Services/Interface/ICatalogService.cs
+++ Frontends/FreeCourse.Web/Services/Interface/ICatalogService.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Web.Models.Catalogs;
 
 namespace FreeCourse.Web.Services.Interface
 {
     public interface ICatalogService
     {
+        Task<List<CourseViewModel>> GetAllCourseAsync();
+        Task<CourseViewModel> GetAllCourseByUserIdAsync(string userId);
+        Task<CourseViewModel> GetByCourseIdAsync(string courseId);
+        Task<bool> CreateCourseAsync(CourseCreateInput courseCreateInput);
+        Task<bool> UpdateCourseAsync(CourseUpdateInput courseUpdateInput);
+        Task<bool> DeleteCourseAsync(string courseId);
 
     }
 }
