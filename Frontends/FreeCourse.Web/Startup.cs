5f6c3997a2b9234b32b6107e4e73e1fc5ecfe19c
muratilgun <muratilgun34@gmail.com>
2022-01-05 23:38:47 +0300

    services.AddAccessTokenManagement();

--- Frontends/FreeCourse.Web/Startup.cs
+++ Frontends/FreeCourse.Web/Startup.cs
@@ -32,12 +32,22 @@ namespace FreeCourse.Web
             services.Configure<ClientSettings>(Configuration.GetSection("ClientSettings"));
             services.Configure<ServiceApiSettings>(Configuration.GetSection("ServiceApiSettings"));
             services.AddHttpContextAccessor();
+            services.AddAccessTokenManagement();
+            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
+
+
+
+
 
             var serviceApiSettings = Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
-            services.AddScoped<ISharedIdentityService, SharedIdentityService>();
-            services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
+
+
+
             services.AddScoped<ResourceOwnerPasswordTokenHandler>();
             services.AddScoped<ClientCredentialTokenHandler>();
+
+
+            services.AddHttpClient<IClientCredentialTokenService, ClientCredentialTokenService>();
             services.AddHttpClient<IIdentityService, IdentityService>();
             services.AddHttpClient<ICatalogService, CatalogService>(opt =>
             {
