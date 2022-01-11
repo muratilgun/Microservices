dc5136777a6fbc2d6a7e4c7c6114d8b10af49990
muratilgun <muratilgun34@gmail.com>
2022-01-11 20:20:33 +0300

    IPhotoStockService

---
+++ Frontends/FreeCourse.Web/Models/PhotoStocks/PhotoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FreeCourse.Web.Models.PhotoStocks
+{
+    public class PhotoViewModel
+    {
+        public string Url { get; set; }
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interface/IPhotoStockService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace FreeCourse.Web.Services.Interface
+{
+    public interface IPhotoStockService
+    {
+        Task<string> UploadPhoto(IFormFile photo);
+    }
+}
