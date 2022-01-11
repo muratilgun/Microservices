3d50a11c01b87d05d846ef194eea9974e57e48cc
muratilgun <muratilgun34@gmail.com>
2022-01-11 22:55:17 +0300

    Update course photo

--- Frontends/FreeCourse.Web/Models/Catalogs/CourseUpdateInput.cs
+++ Frontends/FreeCourse.Web/Models/Catalogs/CourseUpdateInput.cs
@@ -1,20 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace FreeCourse.Web.Models.Catalogs
 {
     public class CourseUpdateInput
     {
         public string Id { get; set; }
+        [Display(Name = "Kurs İsmi")]
+        [Required]
         public string Name { get; set; }
+        [Display(Name = "Kurs Açıklama")]
+        [Required]
         public string Description { get; set; }
+        [Display(Name = "Kurs Fiyat")]
+        [Required]
+
         public decimal Price { get; set; }
         public string UserId { get; set; }
         public string Picture { get; set; }
 
         public FeatureViewModel Feature { get; set; }
+        [Display(Name = "Kurs Kategori")]
+        [Required]
         public string CategoryId { get; set; }
+
+        [Display(Name = "Kurs Resim")]
+        public IFormFile PhotoFormFile { get; set; }
     }
 }
