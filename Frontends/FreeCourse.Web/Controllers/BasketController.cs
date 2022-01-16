047b19ab3735c85f299081e67e19badb31d53f50
muratilgun <muratilgun34@gmail.com>
2022-01-16 12:22:53 +0300

    BasketController

---
+++ Frontends/FreeCourse.Web/Controllers/BasketController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models.Baskets;
+using FreeCourse.Web.Services.Interface;
+
+namespace FreeCourse.Web.Controllers
+{
+    public class BasketController : Controller
+    {
+        private readonly ICatalogService _catalogService;
+        private readonly IBasketService _basketService;
+
+        public BasketController(IBasketService basketService, ICatalogService catalogService)
+        {
+            _basketService = basketService;
+            _catalogService = catalogService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await  _basketService.Get());
+        }
+
+        public async Task<IActionResult> AddBasketItem(string courseId)
+        {
+            var course = await _catalogService.GetByCourseIdAsync(courseId);
+            var basketItem = new BasketItemViewModel
+            {
+                CourseId = courseId,
+                CourseName = course.Name,
+                Price = course.Price,
+            };
+            await _basketService.AddBasketItem(basketItem);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> RemoveBasketItem(string courseId)
+        {
+            await _basketService.RemoveBasketItem(courseId);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
