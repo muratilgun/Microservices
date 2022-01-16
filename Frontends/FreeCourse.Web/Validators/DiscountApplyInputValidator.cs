bdf998d66ba3d43841f7a623741eeb2faab6da14
muratilgun <muratilgun34@gmail.com>
2022-01-16 23:38:58 +0300

    DiscountViewModel & DiscountApplyInput

---
+++ Frontends/FreeCourse.Web/Validators/DiscountApplyInputValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using FreeCourse.Web.Models.Discounts;
+
+namespace FreeCourse.Web.Validators
+{
+    public class DiscountApplyInputValidator : AbstractValidator<DiscountApplyInput>
+    {
+        public DiscountApplyInputValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty().WithMessage("İndirim kupon alanı boş bırakılamaz.");
+        }
+    }
+}
