c78ce6010452424a935613fd87ed5aae004e0db8
muratilgun <muratilgun34@gmail.com>
2022-01-16 23:43:19 +0300

    IDiscountService/DiscountService

---
+++ Frontends/FreeCourse.Web/Services/DiscountService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models.Discounts;
+using FreeCourse.Web.Services.Interface;
+
+namespace FreeCourse.Web.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        public async Task<DiscountViewModel> GetDiscount(string discountCode)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Frontends/FreeCourse.Web/Services/Interface/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models.Discounts;
+
+namespace FreeCourse.Web.Services.Interface
+{
+    public interface IDiscountService
+    {
+        Task<DiscountViewModel> GetDiscount(string discountCode);
+    }
+}
