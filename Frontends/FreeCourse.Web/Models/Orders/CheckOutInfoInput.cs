7d18f3a6664846953d5d009b425be4fffdfa8ebb
muratilgun <muratilgun34@gmail.com>
2022-01-17 23:20:43 +0300

    Create CheckOutInfoInput.cs

---
+++ Frontends/FreeCourse.Web/Models/Orders/CheckOutInfoInput.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Web.Models.Orders
+{
+    public class CheckOutInfoInput
+    {
+        [Display(Name = "il")]
+        public string Province { get; set; }
+        [Display(Name = "ilçe")]
+        public string District { get; set; }
+        [Display(Name = "cadde")]
+        public string Street { get; set; }
+        [Display(Name = "posta kodu")]
+        public string ZipCode { get; set; }
+        [Display(Name = "adres")]
+        public string Line { get; set; }
+        [Display(Name = "kart isim soyisim")]
+        public string CardName { get; set; }
+        [Display(Name = "kart numarası")]
+        public string CardNumber { get; set; }
+        [Display(Name = "son kullanma tarihi(ay/yıl)")]
+        public string Expiration { get; set; }
+        [Display(Name = "CVV/CVC2 numarası")]
+        public string CVV { get; set; }
+    }
+}
