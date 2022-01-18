3b8b3a91510d87d5649ace461e90f1a233e7b15a
muratilgun <muratilgun34@gmail.com>
2022-01-18 21:48:17 +0300

    IOrderService

--- Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderDto.cs
+++ Services/Order/FreeCourse.Services.Order.Application/Dtos/OrderDto.cs
@@ -13,6 +13,6 @@ namespace FreeCourse.Services.Order.Application.Dtos
         public DateTime CreatDateTime { get;  set; }
         public AddressDto Address { get;  set; }
         public string BuyerId { get;  set; }
-        public List<OrderItem> OrderItems { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
     }
 }
