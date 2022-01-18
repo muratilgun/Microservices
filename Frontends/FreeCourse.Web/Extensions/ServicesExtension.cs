6e633a6f8c992bf8a9547d1531fd8c957d030894
muratilgun <muratilgun34@gmail.com>
2022-01-18 21:51:18 +0300

    IOrderService / OrderService configurations

--- Frontends/FreeCourse.Web/Extensions/ServicesExtension.cs
+++ Frontends/FreeCourse.Web/Extensions/ServicesExtension.cs
@@ -44,6 +44,10 @@ namespace FreeCourse.Web.Extensions
             {
                 opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.FakePayment.Path}");
             }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+            services.AddHttpClient<IOrderService, OrderService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GatewayBaseUri}/{serviceApiSettings.Order.Path}");
+            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
         }
     }
 }
---
+++ Frontends/FreeCourse.Web/Services/OrderService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Web.Models.Orders;
+using FreeCourse.Web.Services.Interface;
+
+namespace FreeCourse.Web.Services
+{
+    public class OrderService : IOrderService
+    {
+        public async Task<OrderCreatedViewModel> CreateOrder(CheckOutInfoInput checkOutInfoInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SuspendOrder(CheckOutInfoInput checkOutInfoInput)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<OrderViewModel>> GetOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
