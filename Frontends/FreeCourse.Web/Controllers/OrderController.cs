c3e1068ad80294c8292353210fe3779310ac98c7
muratilgun <muratilgun34@gmail.com>
2022-01-20 23:18:04 +0300

    Added CheckoutHistory

--- Frontends/FreeCourse.Web/Controllers/OrderController.cs
+++ Frontends/FreeCourse.Web/Controllers/OrderController.cs
@@ -47,5 +47,9 @@ namespace FreeCourse.Web.Controllers
             ViewBag.orderId = orderId;
             return View();
         }
+        public async Task<IActionResult> CheckoutHistory()
+        {
+            return View(await _orderService.GetOrder());
+        }
     }
 }
