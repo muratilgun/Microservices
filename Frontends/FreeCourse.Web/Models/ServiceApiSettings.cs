1432bb4f037ede525d0aeb27f5abdd6cb688c5a9
muratilgun <muratilgun34@gmail.com>
2022-01-22 13:47:21 +0300

    Payment Bug Fixed
    FakePayment.Path => Payment.Path

--- Frontends/FreeCourse.Web/Models/ServiceApiSettings.cs
+++ Frontends/FreeCourse.Web/Models/ServiceApiSettings.cs
@@ -14,7 +14,7 @@ namespace FreeCourse.Web.Models
         public ServiceApi PhotoStock { get; set; }
         public ServiceApi Basket { get; set; }
         public ServiceApi Discount { get; set; }
-        public ServiceApi FakePayment { get; set; }
+        public ServiceApi Payment { get; set; }
         public ServiceApi Order { get; set; }
     }
 
